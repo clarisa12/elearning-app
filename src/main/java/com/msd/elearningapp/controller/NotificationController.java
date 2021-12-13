3dee8a1febf710087e199ad62d50c262ca50bfa1
FrancescaVV <francesca.voidoc@gmail.com>
2021-12-13 23:51:56 +0200

    update notification

--- src/main/java/com/msd/elearningapp/controller/NotificationController.java
+++ src/main/java/com/msd/elearningapp/controller/NotificationController.java
@@ -1,79 +1,68 @@
 package com.msd.elearningapp.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msd.elearningapp.domain.Notification;
 import com.msd.elearningapp.exception.ResourceNotFoundException;
 import com.msd.elearningapp.repository.NotificationRepository;
 
-
-@CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/api/v1/")
+class NotificationController {
 
-public class NotificationController {
+  private final NotificationRepository repository;
 
-	@Autowired
-	private NotificationRepository notificationRepository;
-	
-	// get all notifications
-	@GetMapping("/notifications")
-	public List<Notification> getAllNotifications(){
-		return notificationRepository.findAll();
-	}		
-	
-	// create notification rest api
-	@PostMapping("/notifications")
-	public Notification createNotification(@RequestBody Notification notification) {
-		return notificationRepository.save(notification);
-	}
-	
-	// get notification by id rest api
-	@GetMapping("/notifications/{idNotif}")
-	public ResponseEntity<Notification> getNotificationById(@PathVariable Long idNotif) {
-		Notification notification = notificationRepository.findById(idNotif)
-				.orElseThrow(() -> new ResourceNotFoundException("Notification does not exist with id :" + idNotif));
-		return ResponseEntity.ok(notification);
-	}
-	
-	// update notification rest api
-	
-	@PutMapping("/notifications/{idNotif}")
-	public ResponseEntity<Notification> updateNotification(@PathVariable Long idNotif, @RequestBody Notification notificationDetails){
-		Notification notification = notificationRepository.findById(idNotif)
-				.orElseThrow(() -> new ResourceNotFoundException("Notification does not exist with id :" + idNotif));
-		
-		notification.setTxtNotif(notification.getTxtNotif());
-		notification.setDateNotif(notification.getDateNotif());
-		
-		Notification updatedNotification = notificationRepository.save(notification);
-		return ResponseEntity.ok(updatedNotification);
-	}
-	
-	// delete notification rest api
-	@DeleteMapping("/notifications/{idNotif}")
-	public ResponseEntity<Map<String, Boolean>> deleteNotification(@PathVariable Long idNotif){
-		Notification notification = notificationRepository.findById(idNotif)
-				.orElseThrow(() -> new ResourceNotFoundException("Notification does not exist with id :" + idNotif));
-		
-		notificationRepository.delete(notification);
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
-		return ResponseEntity.ok(response);
-	}
-	
+  NotificationController(NotificationRepository repository) {
+    this.repository = repository;
+  }
+
+
+  // Aggregate root
+  // tag::get-aggregate-root[]
+  @GetMapping("/notifications")
+  List<Notification> all() {
+    return repository.findAll();
+  }
+  // end::get-aggregate-root[]
+
+  @PostMapping("/notifications")
+  Notification newNotification(@RequestBody Notification newNotification) {
+    return repository.save(newNotification);
+  }
+
+  // Single item
+  
+  @GetMapping("/notifications/{id}")
+  Notification one(@PathVariable Long id) {
+    
+    return repository.findById(id)
+      .orElseThrow(() -> new ResourceNotFoundException(id));
+  }
+
+  @PutMapping("/notifications/{id}")
+  Notification replaceNotification(@RequestBody Notification newNotification, @PathVariable Long id) {
+    
+    return repository.findById(id)
+      .map(notification -> {
+    	notification.setDateNotif(notification.getDateNotif());
+    	notification.setTxtNotif(notification.getTxtNotif());
+        return repository.save(notification);
+      })
+      .orElseGet(() -> {
+        newNotification.setIdNotif(id);
+        return repository.save(newNotification);
+      });
+  }
+
+  @DeleteMapping("/notifications/{id}")
+  void deleteNotification(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
 }
--- src/main/java/com/msd/elearningapp/domain/Notification.java
+++ src/main/java/com/msd/elearningapp/domain/Notification.java
@@ -1,6 +1,7 @@
 package com.msd.elearningapp.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
@@ -13,7 +14,7 @@ public class Notification {
     private Date dateNotif;
 
     @NotNull  @Id @GeneratedValue
-    private Integer idNotif;
+    private Long idNotif;
 
     @NotEmpty
     private String txtNotif;
@@ -24,10 +25,10 @@ public class Notification {
     public void setDateNotif(Date dateNotif) {
         this.dateNotif = dateNotif;
     }
-    public Integer getIdNotif() {
+    public @NotNull Long getIdNotif() {
         return idNotif;
     }
-    public void setIdNotif(Integer idNotif) {
+    public void setIdNotif(@NotNull Long idNotif) {
         this.idNotif = idNotif;
     }
     public String getTxtNotif() {
@@ -36,10 +37,30 @@ public class Notification {
     public void setTxtNotif(String txtNotif) {
         this.txtNotif = txtNotif;
     }
-	public Notification(Date dateNotif, @NotNull Integer idNotif, @NotEmpty String txtNotif) {
+	public Notification(Date dateNotif, @NotNull @NotNull Long idNotif, @NotEmpty String txtNotif) {
 		super();
 		this.dateNotif = dateNotif;
 		this.idNotif = idNotif;
 		this.txtNotif = txtNotif;
 	}
+	public Notification() {
+		super();
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(dateNotif, idNotif, txtNotif);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Notification other = (Notification) obj;
+		return Objects.equals(dateNotif, other.dateNotif) && Objects.equals(idNotif, other.idNotif)
+				&& Objects.equals(txtNotif, other.txtNotif);
+	}
+	
 }
