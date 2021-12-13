86a5cae5f64f7adb9552b6611cbb1ee532054762
FrancescaVV <francesca.voidoc@gmail.com>
2021-12-13 23:49:34 +0200

    update admin

--- src/main/java/com/msd/elearningapp/domain/Administrator.java
+++ src/main/java/com/msd/elearningapp/domain/Administrator.java
@@ -1,6 +1,7 @@
 package com.msd.elearningapp.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,7 +19,7 @@ public class Administrator extends Person {
     private String adminDepartment;
 
     @Id @NotNull  @GeneratedValue
-    private Integer adminId;
+    private Long adminId;
 
     private String adminTitle;
 
@@ -28,10 +29,10 @@ public class Administrator extends Person {
     public void setAdminDepartment(String adminDepartment) {
         this.adminDepartment = adminDepartment;
     }
-    public Integer getAdminId() {
+    public Long getAdminId() {
         return adminId;
     }
-    public void setAdminId(Integer adminId) {
+    public void setAdminId(Long adminId) {
         this.adminId = adminId;
     }
     public String getAdminTitle() {
@@ -51,11 +52,33 @@ public class Administrator extends Person {
     }
 	public Administrator(@NotNull Long persIdentityNum, @NotNull String persFirstName, @NotNull String persLastName,
 			@Past Date persDoB, @Email @NotNull String persEmail, @NotNull String persNum, String persAdress,
-			String persPhone, @NotEmpty String adminDepartment, @NotNull Integer adminId, String adminTitle) {
+			String persPhone, @NotEmpty String adminDepartment, @NotNull @NotNull Long adminId, String adminTitle) {
 		super(persIdentityNum, persFirstName, persLastName, persDoB, persEmail, persNum, persAdress, persPhone);
 		this.adminDepartment = adminDepartment;
 		this.adminId = adminId;
 		this.adminTitle = adminTitle;
+	}
+	public Administrator() {
+		super();
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(adminDepartment, adminId, adminTitle);
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Administrator other = (Administrator) obj;
+		return Objects.equals(adminDepartment, other.adminDepartment) && Objects.equals(adminId, other.adminId)
+				&& Objects.equals(adminTitle, other.adminTitle);
 	}
     
 }
