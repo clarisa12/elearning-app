2e9bff21d6edb1f9b830e264ecb7bea2200629d9
FrancescaVV <francesca.voidoc@gmail.com>
2021-12-13 23:58:13 +0200

    update task

--- src/main/java/com/msd/elearningapp/controller/TaskController.java
+++ src/main/java/com/msd/elearningapp/controller/TaskController.java
@@ -1,83 +1,72 @@
 package com.msd.elearningapp.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msd.elearningapp.domain.Task;
 import com.msd.elearningapp.exception.ResourceNotFoundException;
 import com.msd.elearningapp.repository.TaskRepository;
 
-
-@CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/api/v1/")
+class TaskController {
 
-public class TaskController {
+  private final TaskRepository repository;
 
-	@Autowired
-	private TaskRepository taskRepository;
-	
-	// get all tasks
-	@GetMapping("/tasks")
-	public List<Task> getAllTasks(){
-		return taskRepository.findAll();
-	}		
-	
-	// create task rest api
-	@PostMapping("/tasks")
-	public Task createTask(@RequestBody Task task) {
-		return taskRepository.save(task);
-	}
-	
-	// get task by id rest api
-	@GetMapping("/tasks/{tskId}")
-	public ResponseEntity<Task> getTaskById(@PathVariable Long tskId) {
-		Task task = taskRepository.findById(tskId)
-				.orElseThrow(() -> new ResourceNotFoundException("Task does not exist with id :" + tskId));
-		return ResponseEntity.ok(task);
-	}
-	
-	// update task rest api
-	
-	@PutMapping("/tasks/{tskId}")
-	public ResponseEntity<Task> updateTask(@PathVariable Long tskId, @RequestBody Task taskDetails){
-		Task task = taskRepository.findById(tskId)
-				.orElseThrow(() -> new ResourceNotFoundException("Task does not exist with id :" + tskId));
-		
-		task.setTskStartDate(task.getTskStartDate());
-		task.setTskEndDate(task.getTskStartDate());
-		task.setTskDescription(task.getTskDescription());
-		task.setTskStudResponsible(task.getTskStudResponsible());
-		task.setTskAssig(task.getTskAssig());
-		task.setTskState(task.getTskState());
-		
-		Task updatedTask = taskRepository.save(task);
-		return ResponseEntity.ok(updatedTask);
-	}
-	
-	// delete task rest api
-	@DeleteMapping("/tasks/{tskId}")
-	public ResponseEntity<Map<String, Boolean>> deleteTask(@PathVariable Long tskId){
-		Task task = taskRepository.findById(tskId)
-				.orElseThrow(() -> new ResourceNotFoundException("Task does not exist with id :" + tskId));
-		
-		taskRepository.delete(task);
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
-		return ResponseEntity.ok(response);
-	}
-	
+  TaskController(TaskRepository repository) {
+    this.repository = repository;
+  }
+
+
+  // Aggregate root
+  // tag::get-aggregate-root[]
+  @GetMapping("/tasks")
+  List<Task> all() {
+    return repository.findAll();
+  }
+  // end::get-aggregate-root[]
+
+  @PostMapping("/tasks")
+  Task newTask(@RequestBody Task newTask) {
+    return repository.save(newTask);
+  }
+
+  // Single item
+  
+  @GetMapping("/tasks/{id}")
+  Task one(@PathVariable Long id) {
+    
+    return repository.findById(id)
+      .orElseThrow(() -> new ResourceNotFoundException(id));
+  }
+
+  @PutMapping("/tasks/{id}")
+  Task replaceTask(@RequestBody Task newTask, @PathVariable Long id) {
+    
+    return repository.findById(id)
+      .map(task -> {
+    	  task.setTskStartDate(task.getTskStartDate());
+  		task.setTskEndDate(task.getTskStartDate());
+  		task.setTskDescription(task.getTskDescription());
+  		task.setTskStudResponsible(task.getTskStudResponsible());
+  		task.setTskAssig(task.getTskAssig());
+  		task.setTskState(task.getTskState());
+        return repository.save(task);
+      })
+      .orElseGet(() -> {
+        newTask.setTskId(id);
+        return repository.save(newTask);
+      });
+  }
+
+  @DeleteMapping("/tasks/{id}")
+  void deleteTask(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
 }
