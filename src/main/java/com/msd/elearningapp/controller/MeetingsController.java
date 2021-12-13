adb72e58e35d6caab649cac16dfda9bebafc8a3e
FrancescaVV <francesca.voidoc@gmail.com>
2021-12-13 23:49:08 +0200

    update meetings

--- src/main/java/com/msd/elearningapp/controller/MeetingsController.java
+++ src/main/java/com/msd/elearningapp/controller/MeetingsController.java
@@ -1,85 +1,74 @@
 package com.msd.elearningapp.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msd.elearningapp.domain.Meetings;
 import com.msd.elearningapp.exception.ResourceNotFoundException;
 import com.msd.elearningapp.repository.MeetingsRepository;
 
-
-@CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/api/v1/")
+class MeetingsController {
 
-public class MeetingsController {
+  private final MeetingsRepository repository;
 
-	@Autowired
-	private MeetingsRepository meetingRepository;
-	
-	// get all meetings
-	@GetMapping("/meetings")
-	public List<Meetings> getAllMeetingss(){
-		return meetingRepository.findAll();
-	}		
-	
-	// create meeting rest api
-	@PostMapping("/meetings")
-	public Meetings createMeetings(@RequestBody Meetings meeting) {
-		return meetingRepository.save(meeting);
-	}
-	
-	// get meeting by id rest api
-	@GetMapping("/meetings/{meetingId}")
-	public ResponseEntity<Meetings> getMeetingsById(@PathVariable Long meetingId) {
-		Meetings meeting = meetingRepository.findById(meetingId)
-				.orElseThrow(() -> new ResourceNotFoundException("Meetings does not exist with id :" + meetingId));
-		return ResponseEntity.ok(meeting);
-	}
-	
-	// update meeting rest api
-	
-	@PutMapping("/meetings/{meetingId}")
-	public ResponseEntity<Meetings> updateMeetings(@PathVariable Long meetingId, @RequestBody Meetings meetingDetails){
-		Meetings meeting = meetingRepository.findById(meetingId)
-				.orElseThrow(() -> new ResourceNotFoundException("Meetings does not exist with id :" + meetingId));
-		
-		meeting.setMeetingStartDate(meeting.getMeetingStartDate());
-		meeting.setMeetingEndDate(meeting.getMeetingEndDate());
-		meeting.setMeetingBody(meeting.getMeetingBody());
-		meeting.setMeetingMembers(meeting.getMeetingMembers());
-		meeting.setMeetingTopic(meeting.getMeetingTopic());
-		meeting.setMeetingState(meeting.getMeetingState());
-		meeting.setMeetingObs(meeting.getMeetingObs());
-		meeting.setMentor(meeting.getMentor());
-		
-		Meetings updatedMeetings = meetingRepository.save(meeting);
-		return ResponseEntity.ok(updatedMeetings);
-	}
-	
-	// delete meeting rest api
-	@DeleteMapping("/meetings/{meetingId}")
-	public ResponseEntity<Map<String, Boolean>> deleteMeetings(@PathVariable Long meetingId){
-		Meetings meeting = meetingRepository.findById(meetingId)
-				.orElseThrow(() -> new ResourceNotFoundException("Meetings does not exist with id :" + meetingId));
-		
-		meetingRepository.delete(meeting);
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
-		return ResponseEntity.ok(response);
-	}
-	
+  MeetingsController(MeetingsRepository repository) {
+    this.repository = repository;
+  }
+
+
+  // Aggregate root
+  // tag::get-aggregate-root[]
+  @GetMapping("/meetings")
+  List<Meetings> all() {
+    return repository.findAll();
+  }
+  // end::get-aggregate-root[]
+
+  @PostMapping("/meetings")
+  Meetings newMeetings(@RequestBody Meetings newMeetings) {
+    return repository.save(newMeetings);
+  }
+
+  // Single item
+  
+  @GetMapping("/meetings/{id}")
+  Meetings one(@PathVariable Long id) {
+    
+    return repository.findById(id)
+      .orElseThrow(() -> new ResourceNotFoundException(id));
+  }
+
+  @PutMapping("/meetings/{id}")
+  Meetings replaceMeetings(@RequestBody Meetings newMeetings, @PathVariable Long id) {
+    
+    return repository.findById(id)
+      .map(meeting -> {
+    	meeting.setMeetingStartDate(meeting.getMeetingStartDate());
+  		meeting.setMeetingEndDate(meeting.getMeetingEndDate());
+  		meeting.setMeetingBody(meeting.getMeetingBody());
+  		meeting.setMeetingMembers(meeting.getMeetingMembers());
+  		meeting.setMeetingTopic(meeting.getMeetingTopic());
+  		meeting.setMeetingState(meeting.getMeetingState());
+  		meeting.setMeetingObs(meeting.getMeetingObs());
+  		meeting.setMentor(meeting.getMentor());
+        return repository.save(meeting);
+      })
+      .orElseGet(() -> {
+        newMeetings.setMeetingId(id);
+        return repository.save(newMeetings);
+      });
+  }
+
+  @DeleteMapping("/meetings/{id}")
+  void deleteMeetings(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
 }
--- src/main/java/com/msd/elearningapp/domain/Meetings.java
+++ src/main/java/com/msd/elearningapp/domain/Meetings.java
@@ -2,6 +2,7 @@ package com.msd.elearningapp.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Date;
 
 import javax.persistence.*;
@@ -36,63 +37,100 @@ public class Meetings {
     private String meetingObs;
     private String meetingTopic;
     private MeetingState meetingState;
+	public long getMeetingId() {
+		return meetingId;
+	}
+	public void setMeetingId(long meetingId) {
+		this.meetingId = meetingId;
+	}
+	public String getMeetingBody() {
+		return meetingBody;
+	}
+	public void setMeetingBody(String meetingBody) {
+		this.meetingBody = meetingBody;
+	}
+	public Date getMeetingStartDate() {
+		return meetingStartDate;
+	}
+	public void setMeetingStartDate(Date meetingStartDate) {
+		this.meetingStartDate = meetingStartDate;
+	}
+	public Date getMeetingEndDate() {
+		return meetingEndDate;
+	}
+	public void setMeetingEndDate(Date meetingEndDate) {
+		this.meetingEndDate = meetingEndDate;
+	}
+	public List<Student> getMeetingMembers() {
+		return meetingMembers;
+	}
+	public void setMeetingMembers(List<Student> meetingMembers) {
+		this.meetingMembers = meetingMembers;
+	}
+	public Professor getMentor() {
+		return mentor;
+	}
+	public void setMentor(Professor mentor) {
+		this.mentor = mentor;
+	}
+	public String getMeetingObs() {
+		return meetingObs;
+	}
+	public void setMeetingObs(String meetingObs) {
+		this.meetingObs = meetingObs;
+	}
+	public String getMeetingTopic() {
+		return meetingTopic;
+	}
+	public void setMeetingTopic(String meetingTopic) {
+		this.meetingTopic = meetingTopic;
+	}
+	public MeetingState getMeetingState() {
+		return meetingState;
+	}
+	public void setMeetingState(MeetingState meetingState) {
+		this.meetingState = meetingState;
+	}
+	public Meetings(@NotNull long meetingId, @Size(min = 1, max = 1000) String meetingBody,
+			@NotNull @Future(message = "Meeting start date must be later than present date!") Date meetingStartDate,
+			@NotNull @Future(message = "Meeting end date must be later than present date!") Date meetingEndDate,
+			List<Student> meetingMembers, Professor mentor, String meetingObs, String meetingTopic,
+			MeetingState meetingState) {
+		super();
+		this.meetingId = meetingId;
+		this.meetingBody = meetingBody;
+		this.meetingStartDate = meetingStartDate;
+		this.meetingEndDate = meetingEndDate;
+		this.meetingMembers = meetingMembers;
+		this.mentor = mentor;
+		this.meetingObs = meetingObs;
+		this.meetingTopic = meetingTopic;
+		this.meetingState = meetingState;
+	}
+	public Meetings() {
+		super();
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(meetingBody, meetingEndDate, meetingId, meetingMembers, meetingObs, meetingStartDate,
+				meetingState, meetingTopic, mentor);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Meetings other = (Meetings) obj;
+		return Objects.equals(meetingBody, other.meetingBody) && Objects.equals(meetingEndDate, other.meetingEndDate)
+				&& meetingId == other.meetingId && Objects.equals(meetingMembers, other.meetingMembers)
+				&& Objects.equals(meetingObs, other.meetingObs)
+				&& Objects.equals(meetingStartDate, other.meetingStartDate) && meetingState == other.meetingState
+				&& Objects.equals(meetingTopic, other.meetingTopic) && Objects.equals(mentor, other.mentor);
+	}
 
 
-    public String getMeetingBody() {
-        return meetingBody;
-    }
-    public void setMeetingBody(String meetingBody) {
-        this.meetingBody = meetingBody;
-    }
-    public Date getMeetingStartDate() {
-        return meetingStartDate;
-    }
-    public void setMeetingStartDate(Date meetingStartDate) {
-        this.meetingStartDate = meetingStartDate;
-    }
-    public Date getMeetingEndDate() {
-        return meetingEndDate;
-    }
-    public void setMeetingEndDate(Date meetingEndDate) {
-        this.meetingEndDate = meetingEndDate;
-    }
-   
-    public Professor getMentor() {
-        return mentor;
-    }
-    public void setMentor(Professor mentor) {
-        this.mentor = mentor;
-    }
-    public String getMeetingObs() {
-        return meetingObs;
-    }
-    public void setMeetingObs(String meetingObs) {
-        this.meetingObs = meetingObs;
-    }
-    public String getMeetingTopic() {
-        return meetingTopic;
-    }
-    public void setMeetingTopic(String meetingTopic) {
-        this.meetingTopic = meetingTopic;
-    }
-
-    public List<Student> getMeetingMembers() {
-        return meetingMembers;
-    }
-    public void setMeetingMembers(List<Student> list) {
-        this.meetingMembers = list;
-    }
-    public MeetingState getMeetingState() {
-        return meetingState;
-    }
-    public void setMeetingState(MeetingState meetingState) {
-        this.meetingState = meetingState;
-    }
-    @Override
-    public String toString() {
-        return "Meetings [meetingId=" + meetingId + ", meetingBody=" + meetingBody + ", meetingStartDate="
-                + meetingStartDate + ", meetingEndDate=" + meetingEndDate + ", meetingMembers=" + meetingMembers
-                + ", mentor=" + mentor +", meetingObs=" + meetingObs + ", meetingTopic=" + meetingTopic
-                + ", meetingState=" + meetingState + "]";
-    }
+    
 }
