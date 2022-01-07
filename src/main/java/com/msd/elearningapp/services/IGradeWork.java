0c8e6a6c05d6d39aab77db8a63dd92a7808d8df0
FrancescaVV <francesca.voidoc@gmail.com>
2022-01-07 23:08:32 +0200

    removed unused classes

--- src/main/java/com/msd/elearningapp/services/IGradeWork.java
+++
@@ -1,9 +0,0 @@
-package com.msd.elearningapp.services;
-
-import com.msd.elearningapp.domain.Grade;
-
-public interface IGradeWork {
-
-    public Grade gradeAssignment();
-    public Grade gradeWorkgroup();
-}
